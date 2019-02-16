333d5cf3cd63cdccac87e30591f9534accaa995c
Aniket Patel <aniketupatel1@gmail.com>
2019-02-16 03:21:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  object = object.key;
+  object = delete object.key;
   return object;
 }
